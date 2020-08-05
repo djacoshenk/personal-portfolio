8bd14625f381c45ba59d07ff2a583daedc5f3605
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-08-04 17:22:42 -0700

    fixed media queries

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const commits = document.querySelector('.commits');
-const commitsBox = document.querySelectorAll('.commits-box');
+
 const colors = ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'];
 
 function getRandomColor() {
@@ -8,7 +8,35 @@ function getRandomColor() {
 }
 
 function fillCommits() {
-  commitsBox.forEach((e) => (e.style.backgroundColor = getRandomColor()));
+  // if a commits-col div exists, run the removeCommits function
+  if (document.querySelector('.commits-col')) {
+    removeCommits();
+  }
+
+  // removeCommits();
+
+  // each loop will create a column and populate it with colored boxes
+  for (let i = 0; i < 20; i++) {
+    // create a column
+    let commitsCol = document.createElement('div');
+    commitsCol.className = 'commits-col';
+    commits.appendChild(commitsCol);
+
+    // create boxes in the column
+    for (let j = 0; j < 7; j++) {
+      let commitsBox = document.createElement('div');
+      commitsBox.className = 'commits-box';
+      commitsBox.style.backgroundColor = getRandomColor();
+      commitsCol.appendChild(commitsBox);
+    }
+  }
+}
+
+function removeCommits() {
+  for (let i = 0; i < 20; i++) {
+    let commitsCol = document.querySelector('.commits-col');
+    commits.removeChild(commitsCol);
+  }
 }
 
 document.addEventListener('load', fillCommits());
