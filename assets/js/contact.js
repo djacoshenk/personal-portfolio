024438364286032b67284ea770ed4eb0a9ad721d
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-08-09 20:08:45 -0700

    removed contact.js and fixed nav bar links

--- assets/js/contact.js
+++
@@ -1,7 +0,0 @@
-const btn = document.querySelector('button');
-btn.addEventListener('click', highlightOnClick);
-
-function highlightOnClick() {
-  btn.style.backgroundColor = '#282a36';
-  btn.style.color = '#ff79c6';
-}
