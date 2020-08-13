dc5e6f946250c5dda2ff0b56eeff24b51ffebc0b
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-08-12 19:39:11 -0700

    created regex for email

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -5,6 +5,7 @@ const message = document.getElementById('message');
 const form = document.getElementById('contact-form');
 const formInput = document.getElementsByTagName('input');
 const formTextArea = document.getElementsByTagName('textarea');
+const emailFormat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 function validateForm(e) {
   let errors = 0;
@@ -14,7 +15,11 @@ function validateForm(e) {
     name.parentElement.lastElementChild.innerHTML = 'Please enter a name.';
   }
 
-  if (email.value === '' || email.value == null) {
+  if (
+    email.value === '' ||
+    email.value == null ||
+    !email.value.match(emailFormat)
+  ) {
     errors++;
     email.parentElement.lastElementChild.innerHTML =
       'Please enter a valid email.';
@@ -40,28 +45,39 @@ function validateForm(e) {
 function formListener(e) {
   let target = e.currentTarget;
 
-  if (target.value === '' || name.value == null) {
-    switch (target) {
-      case name:
-        name.parentElement.lastElementChild.innerHTML = 'Please enter a name.';
-        break;
-      case email:
-        email.parentElement.lastElementChild.innerHTML =
-          'Please enter a valid email.';
-        break;
-      case subject:
-        subject.parentElement.lastElementChild.innerHTML =
-          'Please enter a subject.';
-        break;
-      case message:
-        message.parentElement.lastElementChild.innerHTML =
-          'Please enter a message.';
-        break;
+  if (target === name) {
+    if (target.value === '' || name.value == null) {
+      name.parentElement.lastElementChild.innerHTML = 'Please enter a name.';
+    } else {
+      name.parentElement.lastElementChild.innerHTML = '';
     }
   }
 
-  if (target.value.length > 0) {
-    target.parentElement.lastElementChild.innerHTML = '';
+  if (target === email) {
+    if (!email.value.match(emailFormat)) {
+      email.parentElement.lastElementChild.innerHTML =
+        'Please enter a valid email.';
+    } else {
+      email.parentElement.lastElementChild.innerHTML = '';
+    }
+  }
+
+  if (target === subject) {
+    if (target.value === '' || name.value == null) {
+      subject.parentElement.lastElementChild.innerHTML =
+        'Please enter a subject.';
+    } else {
+      subject.parentElement.lastElementChild.innerHTML = '';
+    }
+  }
+
+  if (target === message) {
+    if (target.value === '' || name.value == null) {
+      message.parentElement.lastElementChild.innerHTML =
+        'Please enter a message.';
+    } else {
+      message.parentElement.lastElementChild.innerHTML = '';
+    }
   }
 }
 
