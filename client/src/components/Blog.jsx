89cfe074ed430ab7319a501abbeeb32fa11101c0
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-08-28 00:09:08 -0700

    changed sass

--- client/src/components/Blog.jsx
+++ client/src/components/Blog.jsx
@@ -41,21 +41,7 @@ function Blog() {
           </p>
         </a>
         <a className='blog-post' href='#home'>
-          <h3>Array(), Array.from(), & Array.of()</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-            distinctio delectus accusamus ea, magnam velit!
-          </p>
-        </a>
-        <a className='blog-post' href='#home'>
           <h3>Function Declarations & Expressions</h3>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
-            distinctio delectus accusamus ea, magnam velit!
-          </p>
-        </a>
-        <a className='blog-post' href='#home'>
-          <h3>What are Identifiers?</h3>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda
             distinctio delectus accusamus ea, magnam velit!
