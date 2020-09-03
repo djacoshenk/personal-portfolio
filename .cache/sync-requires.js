dc617d8d31c510e64c6f020f6fbd07746c6dd264
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-09-03 15:08:38 -0700

    updated public folder

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/djacoshenk/Desktop/Projects/personal-portfolio/.cache/dev-404-page.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/djacoshenk/Desktop/Projects/personal-portfolio/src/pages/index.js")))
 }
 
