893530fdcb0cf67da5a8e3e97e0b1a148ce7694f
Danny Jacoshenk <daniel.jacoshenk@gmail.com>
2020-09-06 18:41:26 -0700

    added sass script

--- .cache/api-runner-browser-plugins.js
+++ .cache/api-runner-browser-plugins.js
@@ -1,8 +1,8 @@
 module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-fontawesome-css/gatsby-browser.js'),
+      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
-      plugin: require('../node_modules/gatsby-plugin-catch-links/gatsby-browser.js'),
+      plugin: require('../node_modules/gatsby-plugin-fontawesome-css/gatsby-browser.js'),
       options: {"plugins":[]},
     },{
       plugin: require('../gatsby-browser.js'),
