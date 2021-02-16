f278111cf5243df3f39d9946c2fd3607e1d4d15b
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-15 20:45:43 -0800

    create NavBar component

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,11 +1,34 @@
+import { useState } from 'react';
+
 import logoBlack from 'assets/logo-black.svg';
+import menuBlack from 'assets/menu-black.svg';
 
 import './NavBar.scss';
 
 export const NavBar = () => {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
   return (
     <div className='nav-bar-container'>
       <img src={logoBlack} className='logo' alt='logo' />
+      <button
+        className='hamburger-menu'
+        onClick={() => setMenuIsOpen(!menuIsOpen)}
+      >
+        <img src={menuBlack} className='menu-logo' alt='menu-logo' />
+      </button>
+      {menuIsOpen && (
+        <div className='nav-bar-links-mobile'>
+          <a href='#home'>Home</a>
+          <a href='#work'>Work</a>
+          <a href='#contact'>Contact</a>
+        </div>
+      )}
+      <div className='nav-bar-links-dom'>
+        <a href='#home'>Home</a>
+        <a href='#work'>Work</a>
+        <a href='#contact'>Contact</a>
+      </div>
     </div>
   );
 };
