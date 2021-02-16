f3816b80d8effdb96dc0d04496b642b338d8e2a8
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-15 20:46:40 -0800

    create main App with styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { Fragment } from 'react';
 
 import { NavBar } from './components/NavBar';
+import { HomePage } from './components/HomePage';
 
 export const App = () => {
   return (
     <Fragment>
       <NavBar />
+      <HomePage />
     </Fragment>
   );
 };
