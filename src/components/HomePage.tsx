be97c19444b283d2730f5e19e36aedf310a4942f
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-15 20:46:10 -0800

    create HomePage component

---
+++ src/components/HomePage.tsx
@@ -0,0 +1,10 @@
+import './HomePage.scss';
+
+export const HomePage = () => {
+  return (
+    <div className='home-page-container' id='#home'>
+      <h1>Hi, my name is Danny Jacoshenk</h1>
+      <h1>I&#39;m a Frontend Developer</h1>
+    </div>
+  );
+};
