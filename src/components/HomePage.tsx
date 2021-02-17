e56c1ca64c8a99c16cee51bf8cb5bbf8f6d37424
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-17 12:39:19 -0800

    Add Commits component to HomePage component

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,10 +1,13 @@
+import { Commits } from './Commits';
+
 import './HomePage.scss';
 
 export const HomePage = () => {
   return (
     <div className='home-page-container' id='#home'>
-      <h1>Hi, my name is Danny Jacoshenk</h1>
-      <h1>I&#39;m a Frontend Developer</h1>
+      <Commits />
+      <h1>Hi, my name is Danny.</h1>
+      <h1>I&#39;m a Front-End Developer.</h1>
     </div>
   );
 };
