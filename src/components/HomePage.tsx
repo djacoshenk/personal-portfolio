6682f4aec933e97c9bf54587cd58ad6847b4d969
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-19 09:29:35 -0800

    Move Commits component position on Home Page

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -5,9 +5,11 @@ import './HomePage.scss';
 export const HomePage = () => {
   return (
     <div className='home-page-container' id='#home'>
+      <div className='home-page-title'>
+        <h1>Hi, my name is Danny.</h1>
+        <h1>I&#39;m a Front-End Developer.</h1>
+      </div>
       <Commits />
-      <h1>Hi, my name is Danny.</h1>
-      <h1>I&#39;m a Front-End Developer.</h1>
     </div>
   );
 };
