03088235c6f7638aec229bdea154227d37dcbe9b
Danny Jacoshenk <hello@dannyjaco.me>
2021-02-20 15:37:59 -0800

    Create dark/light theme button (#17)
    * Update README.md
    
    * Remove packages for cypress
    
    * Remove types for cypress from tsconfig.json
    
    * Move Commits component position on Home Page
    
    * Reduce padding on NavBar component
    
    * Fix Commits component styling for mobile
    
    * Reduce padding on ContactBar component
    
    * Hide overflow on main styling
    
    * Create Redux store for theme state
    
    * Add Redux and Redux DevTools
    
    * Add Redux Provider to App
    
    * Add dark/light theme styles
    
    * Create reducer for theme state
    
    * Adjust HomePage component styles
    
    * Create toggleable social links per theme
    
    * Improve theme toggle on NavBar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { Fragment } from 'react';
+import { Provider } from 'react-redux';
 
 import { NavBar } from './components/NavBar';
 import { HomePage } from './components/HomePage';
 import { ContactBar } from './components/ContactBar';
 
+import { store } from 'store/index';
+
 export const App = () => {
   return (
     <Fragment>
-      <NavBar />
-      <HomePage />
-      <ContactBar />
+      <Provider store={store}>
+        <NavBar />
+        <HomePage />
+        <ContactBar />
+      </Provider>
     </Fragment>
   );
 };
--- src/components/ContactBar.tsx
+++ src/components/ContactBar.tsx
@@ -1,17 +1,33 @@
+import { useSelector, TypedUseSelectorHook } from 'react-redux';
+
+import { RootState } from 'store/index';
+
 import './ContactBar.scss';
 
+const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+
 export const ContactBar = () => {
+  const { theme } = useTypedSelector((state) => state.theme);
+
   return (
     <div className='contact-bar-container'>
       <a href='https://github.com/djacoshenk' target='_blank' rel='noreferrer'>
-        <i className='fab fa-github'></i>
+        {theme === 'light' ? (
+          <i id='github-logo' className='fab fa-github light'></i>
+        ) : (
+          <i id='github-logo' className='fab fa-github dark'></i>
+        )}
       </a>
       <a
         href='https://linkedin.com/in/djacoshenk'
         target='_blank'
         rel='noreferrer'
       >
-        <i className='fab fa-linkedin-in'></i>
+        {theme === 'light' ? (
+          <i id='linkedin-logo' className='fab fa-linkedin-in light'></i>
+        ) : (
+          <i id='linkedin-logo' className='fab fa-linkedin-in dark'></i>
+        )}
       </a>
     </div>
   );
