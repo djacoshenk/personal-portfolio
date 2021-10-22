187ec16b787894c3b74c68e007f961ec4f173263
Danny Jacoshenk <hello@dannyjaco.me>
2021-10-22 14:35:36 -0700

    Refactor Commits component using tailwind css styles

--- src/components/Commits.tsx
+++ src/components/Commits.tsx
@@ -1,8 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { v4 as uuid } from 'uuid';
-
-import 'components/Commits.css';
+import { v4 as uuid } from "uuid";
 
 export function Commits() {
   const [tick, setTick] = useState(0);
@@ -11,7 +9,7 @@ export function Commits() {
   const COMMMITSBOX = Array(7).fill(0);
 
   function getRandomColor() {
-    const COLORS = ['#ebedf0', '#9be9a8', '#40c463', '#30a14e', '#216e39'];
+    const COLORS = ["bg-gray-50", "bg-green-300", "bg-green-400", "bg-green-500", "bg-green-600"];
     const randomIndex = Math.floor(Math.random() * COLORS.length);
     return COLORS[randomIndex];
   }
@@ -25,22 +23,18 @@ export function Commits() {
   }, [tick]);
 
   return (
-    <div className='commits-container'>
-      {COMMITSCOL.map(() => {
-        return (
-          <div className='commits-col' key={uuid()}>
-            {COMMMITSBOX.map(() => {
-              return (
-                <div
-                  className='commits-box'
-                  key={uuid()}
-                  style={{ backgroundColor: getRandomColor() }}
-                ></div>
-              );
-            })}
-          </div>
-        );
-      })}
-    </div>
+    <>
+      <div className="hidden w-full flex items-center justify-center py-6 overflow-x-hidden xl:flex">
+        {COMMITSCOL.map(() => {
+          return (
+            <div className="w-min flex flex-col items-center justify-center px-1" key={uuid()}>
+              {COMMMITSBOX.map(() => {
+                return <div className={`h-2.5 w-2.5 my-1 rounded-sm ${getRandomColor()}`} key={uuid()}></div>;
+              })}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
